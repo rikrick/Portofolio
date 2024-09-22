77c9db7bd947f50abaf910b239fb2b16b42cce44
rikrick <hotarutrick@gmail.com>
2024-09-22 23:52:22 +0700

    added portofolio file

---
+++ Script.js
@@ -0,0 +1,10 @@
+var icon = document.getElementById("icon");
+
+icon.onclick = function () {
+  document.body.classList.toggle("darkmode");
+  if (document.body.classList.contains("darkmode")) {
+    icon.src = "../Img/sun.svg";
+  } else {
+    icon.src = "../Img/moon.svg";
+  }
+};
