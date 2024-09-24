03fa04c6cfe9e2ad51a769bfb1d6dde4d5142e85
rikrick <hotarutrick@gmail.com>
2024-09-24 21:43:52 +0700

    updated resume button

--- Script.js
+++ Script.js
@@ -3,8 +3,8 @@ var icon = document.getElementById("icon");
 icon.onclick = function () {
   document.body.classList.toggle("darkmode");
   if (document.body.classList.contains("darkmode")) {
-    icon.src = "../Img/sun.svg";
+    icon.src = "Img/sun.svg";
   } else {
-    icon.src = "../Img/moon.svg";
+    icon.src = "Img/moon.svg";
   }
 };
